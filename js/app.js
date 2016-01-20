0e657a0f9f69926c328d224ed38209b8119f949c
Denis Moiseev <isdenmois@gmail.com>
2016-01-21 02:10:25 +0300

    Recreate layout with JADE template engine

--- js/app.js
+++ js/app.js
@@ -4,39 +4,17 @@ var Skipjack = require('./lib/skipjack');
 var ECBMode = require('./modes/ecb');
 var OFBMode = require('./modes/ofb');
 
-function encrypt(cipher, data) {
-    data = cipher.encrypt(ua.to8Array(data));
-    return b64.encode(data);
-}
-
-function decrypt(cipher, data) {
-    data = cipher.decrypt(b64.decode(data));
-    return ua.a8toString(data);
-}
-
-function getValue(id) {
-    return document.getElementById(id).value;
-}
-
-function getRValue(name) {
-    return document.querySelector('input[name="' + name + '"]:checked').value;
-}
-
-function setValue(id, value) {
-    document.getElementById(id).innerHTML = value;
-}
-
-function process(event) {
+/**
+ * Encrypt the message.
+ */
+function encrypt(event) {
     event.preventDefault();
-    document.getElementById('result-panel').removeAttribute('class');
+    var toEncrypt = $('#encrypt').val();
+    var pkey = $('#pkey').val();
+    var iv = ua.toArray($('#iv').val());
+    var mode = $('input[name="mode"]:checked').attr('id');
 
-    var toEncrypt = parseInt(getRValue('encrypt'));
-    var data = getValue('data');
-    var pass = getValue('password');
-    var iv = ua.toArray(getValue('iv'));
-    var mode = getRValue('mode');
-
-    var cipher = new Skipjack(pass);
+    var cipher = new Skipjack(pkey);
     switch (mode) {
         case 'ofb':
             mode = new OFBMode(cipher, iv);
@@ -46,13 +24,48 @@ function process(event) {
             mode = new ECBMode(cipher);
     }
 
-    var result = toEncrypt ? encrypt(mode, data) : decrypt(mode, data);
-    setValue('result', result);
+    toEncrypt = ua.to8Array(toEncrypt);
+    toEncrypt = mode.encrypt(toEncrypt);
+    var result = b64.encode(toEncrypt);
+    $('#decrypt').val(result).find('+ label').addClass('active');
 }
 
+/**
+ * Decrypt the message.
+ */
+function decrypt(event) {
+    event.preventDefault();
+    var toDecrypt = $('#decrypt').val();
+    var pkey = $('#pkey').val();
+    var iv = ua.toArray($('#iv').val());
+    var mode = $('input[name="mode"]:checked').attr('id');
+
+    var cipher = new Skipjack(pkey);
+    switch (mode) {
+        case 'ofb':
+            mode = new OFBMode(cipher, iv);
+            break;
+
+        default:
+            mode = new ECBMode(cipher);
+    }
+
+    toDecrypt = b64.decode(toDecrypt);
+    toDecrypt = mode.decrypt(toDecrypt);
+    var result = ua.a8toString(toDecrypt);
+    $('#encrypt').val(result).find('+ label').addClass('active');
+}
+
+/**
+ * Add listeners.
+ */
 window.onload = function () {
-    var form = document.getElementById('process-form');
-    if (form && form.addEventListener) {
-        form.addEventListener('submit', process);
+    var encryptBtn = document.getElementById('encryptBtn');
+    if (encryptBtn) {
+        encryptBtn.onclick = encrypt;
+    }
+    var decryptBtn = document.getElementById('decryptBtn');
+    if (decryptBtn && decryptBtn.addEventListener) {
+        decryptBtn.onclick = decrypt;
     }
 };
